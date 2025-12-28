c98d5c86cdb8f5929fac8525dd53395578e0e286
mido091 <midoproblems091@gmail.com>
2025-12-28 19:45:26 +0200

    Full project update

--- controller/plog.controller.js
+++ controller/plog.controller.js
@@ -32,8 +32,8 @@ exports.getPlogById = async (req, res) => {
 //create plog
 exports.createPlog = async (req, res) => {
   try {
-    const { header, title, description, footer } = req.body;
-    if (!header || !title || !description) {
+    const { header, title, description, footer , category } = req.body;
+    if (!header || !title || !description || !category) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
@@ -45,8 +45,8 @@ exports.createPlog = async (req, res) => {
     const filePath = image.path;
 
     const [result] = await db.query(
-      "INSERT INTO plogs (header , title , image , description , footer ) VALUES ( ? , ? , ? , ? , ? )",
-      [header, title, filePath, description, footer]
+      "INSERT INTO plogs (header , title , image , description , footer , category ) VALUES ( ? , ? , ? , ? , ? , ? )",
+      [header, title, filePath, description, footer , category]
     );
     if (result.affectedRows === 0) {
       return res.status(400).json({ message: "Plog not created" });
@@ -62,7 +62,7 @@ exports.createPlog = async (req, res) => {
 // update plog
 exports.updatePlog = async (req, res) => {
   try {
-    const { header, title, description, footer } = req.body;
+    const { header, title, description, footer , category } = req.body;
     const id = req.params.id;
     const image = req.file;
 
@@ -73,13 +73,14 @@ exports.updatePlog = async (req, res) => {
     const currentImage = rows[0].image;
     const filePath = image ? image.path : currentImage;
     const query =
-      "UPDATE plogs SET header = ? , title = ? , image = ? , description = ? , footer = ? WHERE id = ?";
+      "UPDATE plogs SET header = ? , title = ? , image = ? , description = ? , footer = ? , category = ? WHERE id = ?";
     const [result] = await db.query(query, [
       header,
       title,
       filePath,
       description,
       footer,
+      category,
       id,
     ]);
     if (result.affectedRows === 0) {
